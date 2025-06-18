ec0dd482a73f70fe48323884680cf1343f4388d2
matthew-heartful <matthew.heartful@gmail.com>
2025-06-17 23:10:40 -0700

    formatting and fixing build

--- src/app/low-level/[userId]/workflow/page.tsx
+++ src/app/low-level/[userId]/workflow/page.tsx
@@ -4,7 +4,6 @@ import { useState } from 'react';
 import { Button } from '@/components/ui/button';
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Textarea } from '@/components/ui/textarea';
-import { Avatar, AvatarFallback } from "@/components/ui/avatar"
 import { Paperclip, Send, PlusCircle, Trash2 } from "lucide-react"
 import { Input } from "@/components/ui/input"
 
