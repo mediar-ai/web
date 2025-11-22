f0d81248c465a9bdf2ddc756ed5af616103ea9b4
louis030195 <louis.beaumont@gmail.com>
2025-11-21 17:36:28 -0800

    style: Increase modal width to prevent tab overflow

--- src/components/deployments/UnifiedWorkflowDialog.tsx
+++ src/components/deployments/UnifiedWorkflowDialog.tsx
@@ -625,7 +625,7 @@ export function UnifiedWorkflowDialog({
 
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
-      <DialogContent className="max-w-4xl h-[85vh] flex flex-col rounded-2xl border-0 shadow-2xl p-0 overflow-hidden">
+      <DialogContent className="max-w-6xl h-[85vh] flex flex-col rounded-2xl border-0 shadow-2xl p-0 overflow-hidden">
         <DialogHeader>
           {/* Inline Editable Title */}
           {isEditingName ? (
