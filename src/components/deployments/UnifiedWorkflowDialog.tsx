24db059417a4c717e9094d3b17ce124ef942c00d
louis030195 <louis.beaumont@gmail.com>
2025-11-24 16:23:29 -0800

    feat: sort machines by health status in admin panel (healthy first)

--- src/components/deployments/UnifiedWorkflowDialog.tsx
+++ src/components/deployments/UnifiedWorkflowDialog.tsx
@@ -1191,7 +1191,7 @@ export function UnifiedWorkflowDialog({
                       cronMaxConcurrent={cronConfig.maxConcurrent}
                       cronRetryOnFailure={cronConfig.retryOnFailure}
                       cronRetryCount={cronConfig.retryCount}
-                      onChange={(newConfig) => setCronConfig({ ...newConfig, executorType: cronConfig.executorType })}
+                      onChange={(newConfig) => setCronConfig({ ...newConfig, executorType: cronConfig.executorType || 'python' })}
                       showAdvanced={true}
                     />
 
