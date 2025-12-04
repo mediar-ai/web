45cef32b7d943e93132821d0a83a0f4cbd7ba7bb
m13v <i@m13v.com>
2025-12-04 12:48:00 -0800

    Revert "feat: add Daytona sandbox API and user sandbox management"
    This reverts commit 8fcae6026540e105805e0a887fc07a2651e45a13.

--- run-cleanup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /c/Users/matt/mediar-web-app-workspace2
-export DAYTONA_API_KEY=$(grep "^DAYTONA_API_KEY=" .env.local | cut -d '=' -f2 | tr -d '"')
-echo "Using API key: ${DAYTONA_API_KEY:0:10}..."
-node cleanup-sandboxes.mjs
